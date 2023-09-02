f86e4fe040b7dbea8aa2b1ccfc2039b59f92c978
Brian Cairl <briancairl@gmail.com>
2023-09-02 19:39:41 -0400

    Normlize vertex IDs during extract.py

--- test_data/extract.py
+++ test_data/extract.py
@@ -1,19 +1,20 @@
 #!/bin/python3
 import argparse
-import numpy as np
 import os
-import pyrosm
-import matplotlib.pyplot as plt
 import json
+import pyrosm
+import geopy.distance
+
 
 def main():
   parser = argparse.ArgumentParser(description='Creates a graph from a black/white map image')
   parser.add_argument('place', help='Place to download OSM data for', type=str)
   parser.add_argument('--dir', help='Network data directory', type=str, default="/tmp/pyrosm/", required=False)
+  parser.add_argument('--force', '-f', help='Force reprocessing', action='store_true')
   args = parser.parse_args()
 
   json_file_path = os.path.join(args.dir, f"{args.place}.json");
-  if os.path.exists(json_file_path):
+  if not args.force and os.path.exists(json_file_path):
     print(f"{json_file_path} exists")
     return 0
 
@@ -25,18 +26,22 @@ def main():
   nodes, edges = osm.get_network(network_type='driving', nodes=True)
 
   g_nodes = []
+  g_node_id_map = {}
   for id, lat, lon in zip(nodes.id, nodes.lat, nodes.lon):
+    g_node_id_map[id] = len(g_nodes)
     g_nodes.append({
-      "n" : id,
-      "x" : lat,
-      "y" : lon,
+      "lat" : lat,
+      "lon" : lon,
     })
 
   g_edges = []
   for u, v, length in zip(edges.u, edges.v, edges.length):
+    u_n = g_node_id_map[u]
+    v_n = g_node_id_map[v]
     g_edges.append({
-      "u" : u,
-      "v" : v,
+      "u" : u_n,
+      "v" : v_n,
+      "w" : geopy.distance.geodesic((g_nodes[u_n]["lat"], g_nodes[u_n]["lon"]), (g_nodes[v_n]["lat"], g_nodes[v_n]["lon"])).m
     })
 
   with open(json_file_path, 'w+', encoding='utf-8') as wf:
